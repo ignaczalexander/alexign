d374822e6b135b827fb39694a0046f56950ee3aa
Alexander Ignacz <ignaczalexander@gmail.com>
2020-03-07 15:16:48 +0100

    Updates

---
+++ precache-manifest.3eac7dae54c7147dd99b4ed184db8fc7.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8799e0ea3f749fd9e2aacd2790acd31",
+    "url": "/alexign/index.html"
+  },
+  {
+    "revision": "352a5374faf6d5465b28",
+    "url": "/alexign/static/css/main.fb456979.chunk.css"
+  },
+  {
+    "revision": "33d3a0f01c0a688d2c18",
+    "url": "/alexign/static/js/2.c4d4822f.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/alexign/static/js/2.c4d4822f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "352a5374faf6d5465b28",
+    "url": "/alexign/static/js/main.71079427.chunk.js"
+  },
+  {
+    "revision": "0227576dde36ba9c60c5",
+    "url": "/alexign/static/js/runtime-main.92425282.js"
+  },
+  {
+    "revision": "d91be94737c017af8def153e00677322",
+    "url": "/alexign/static/media/undraw_message_sent_1030.d91be947.svg"
+  },
+  {
+    "revision": "5a78f7e6a46a5ff3deb7823ff271f55d",
+    "url": "/alexign/static/media/undraw_programming_2svr.5a78f7e6.svg"
+  },
+  {
+    "revision": "1fef6459f4384abf7754a8617ca08ca9",
+    "url": "/alexign/static/media/undraw_progressive_app_m9ms.1fef6459.svg"
+  },
+  {
+    "revision": "dba0495542395d7ce855624723a273c1",
+    "url": "/alexign/static/media/undraw_wordpress_utxt.dba04955.svg"
+  }
+]);
