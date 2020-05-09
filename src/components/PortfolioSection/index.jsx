5fe43842de382535f9bf0a62395a72f314c481d0
Alexander Ignacz <ignaczalexander@gmail.com>
2020-05-09 15:36:33 +0200

    Change portfolio item

--- src/components/PortfolioSection/index.jsx
+++ src/components/PortfolioSection/index.jsx
@@ -1,8 +1,43 @@
 import React from "react";
 import styles from "./portfolio.module.scss";
 import Titles from "../shared/Titles";
+import tmwThumbnail from "../../assets/img/trackmywork-thumbnail.png";
 
+const PortfolioItem = props => {
+  const { url, thumbnail, description } = props;
+  return (
+    <a
+      className={styles.element}
+      href={url}
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <img src={thumbnail} alt="" />
+
+      <div className={styles.overlay}>
+        <div>{description}</div>
+      </div>
+    </a>
+  );
+};
 const PortfolioSection = () => {
+  const portfolioItems = [
+    {
+      description: "A minimalistic work shift tracker",
+      thumbnail: tmwThumbnail,
+      url: "https://tmwapp.herokuapp.com"
+    },
+    {
+      description: "A minimalistic work shift tracker",
+      thumbnail: tmwThumbnail,
+      url: "https://tmwapp.herokuapp.com"
+    },
+    {
+      description: "A minimalistic work shift tracker",
+      thumbnail: tmwThumbnail,
+      url: "https://tmwapp.herokuapp.com"
+    }
+  ];
   return (
     <div className={styles.container}>
       <Titles
@@ -10,16 +45,13 @@ const PortfolioSection = () => {
         subtitle="Take a look at our featured work"
       />
       <div className={styles.elements}>
-        <div className={styles.element}>
-          <a
-            href="https://tmwapp.herokuapp.com"
-            target="_blank"
-            rel="noopener noreferrer"
-          ></a>
-        </div>
-        <div className={styles.element}></div>
-        <div className={styles.element}></div>
-        <div className={styles.element}></div>
+        {portfolioItems.map(item => (
+          <PortfolioItem
+            description={item.description}
+            thumbnail={item.thumbnail}
+            url={item.url}
+          />
+        ))}
       </div>
     </div>
   );
