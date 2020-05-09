b6f0c0aaf3c98697fbe7ccd967644c4115d28d53
Alexander Ignacz <ignaczalexander@gmail.com>
2020-05-09 15:36:41 +0200

    Change features component

--- src/components/FeaturesSection/index.jsx
+++ src/components/FeaturesSection/index.jsx
@@ -3,30 +3,46 @@ import styles from "./features.module.scss";
 import Titles from "../shared/Titles";
 
 const FeaturesSection = () => {
+  const frontendSkills = [
+    "React.js",
+    "Redux",
+    "HTML/CSS",
+    "Node.js",
+    "Gatsby",
+    "Vue.js",
+  ];
   return (
     <div className={styles.container}>
       <div className={styles.feature_item}>
-        <i className="fab fa-wordpress-simple"></i>
-        <h3>WordPress websites</h3>
-        <div>
+        <i className="fas fa-code"></i>
+        <h3>Front End</h3>
+        <div className={styles.description}>
+          Nullam urna sem, vehicula vitae viverra vel, eleifend vel urna. Ut
+          vulputate dui a dui tempus, sit amet gravida odio suscipit. Aliquam
+          quis eros et dui dignissim.
+        </div>
+        <div className={styles.skill_list}>
+          {frontendSkills.map((skill) => (
+            <div className={styles.list_item}>
+              <div className={styles.dot}></div>
+              <div>{skill}</div>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className={styles.feature_item}>
+        <i className="fas fa-terminal"></i>
+        <h3>Back End</h3>
+        <div className={styles.description}>
           Nullam urna sem, vehicula vitae viverra vel, eleifend vel urna. Ut
           vulputate dui a dui tempus, sit amet gravida odio suscipit. Aliquam
           quis eros et dui dignissim.
         </div>
       </div>
       <div className={styles.feature_item}>
-        <i className="fas fa-mobile-alt"></i>
-        <h3>WordPress websites</h3>
-        <div>
-          Nullam urna sem, vehicula vitae viverra vel, eleifend vel urna. Ut
-          vulputate dui a dui tempus, sit amet gravida odio suscipit. Aliquam
-          quis eros et dui dignissim.
-        </div>
-      </div>
-      <div className={styles.feature_item}>
-        <i className="fas fa-envelope"></i>
-        <h3>WordPress websites</h3>
-        <div>
+        <i className="fab fa-apple"></i>
+        <h3>iOS development</h3>
+        <div className={styles.description}>
           Nullam urna sem, vehicula vitae viverra vel, eleifend vel urna. Ut
           vulputate dui a dui tempus, sit amet gravida odio suscipit. Aliquam
           quis eros et dui dignissim.
