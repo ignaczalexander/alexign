8842d1b5198306c0add1bc29c6f066e8d89924d4
Alexander Ignacz <ignaczalexander@gmail.com>
2020-05-11 14:08:19 +0200

    Change feature section

--- src/components/FeaturesSection/index.jsx
+++ src/components/FeaturesSection/index.jsx
@@ -1,6 +1,34 @@
 import React from "react";
 import styles from "./features.module.scss";
-import Titles from "../shared/Titles";
+import ScrollAnimation from "react-animate-on-scroll";
+import "animate.css/animate.min.css";
+import Swiper from "react-id-swiper";
+import "swiper/css/swiper.css";
+import "./swiper.css";
+
+const FeatureItem = (props) => {
+  const { skills, icon, title, description, color } = props;
+  return (
+    <ScrollAnimation animateOnce duration={0.5} animateIn="animate__zoomIn">
+      <div className={styles.feature_item}>
+        <i style={{ color: color }} className={icon}></i>
+        <h3>{title}</h3>
+        <div className={styles.description}>{description}</div>
+        <div className={styles.skill_list}>
+          {skills.map((skill) => (
+            <div className={styles.list_item}>
+              <div
+                style={{ backgroundColor: color }}
+                className={styles.dot}
+              ></div>
+              <div>{skill}</div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </ScrollAnimation>
+  );
+};
 
 const FeaturesSection = () => {
   const frontendSkills = [
@@ -11,43 +39,79 @@ const FeaturesSection = () => {
     "Gatsby",
     "Vue.js",
   ];
+  const backendSkills = [
+    "React.js",
+    "Redux",
+    "HTML/CSS",
+    "Node.js",
+    "Gatsby",
+    "Vue.js",
+  ];
+  const iosSkills = [
+    "React.js",
+    "Redux",
+    "HTML/CSS",
+    "Node.js",
+    "Gatsby",
+    "Vue.js",
+  ];
+
+  const params = {
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true,
+    },
+    slidesPerView: 1,
+    watchOverflow: true,
+    breakpoints: {
+      1024: {
+        slidesPerView: 3,
+        spaceBetween: 0,
+      },
+    },
+  };
+
   return (
     <div className={styles.container}>
-      <div className={styles.feature_item}>
-        <i className="fas fa-code"></i>
-        <h3>Front End</h3>
-        <div className={styles.description}>
-          Nullam urna sem, vehicula vitae viverra vel, eleifend vel urna. Ut
+      <Swiper {...params}>
+        <div>
+          <FeatureItem
+            title="Front End"
+            icon="fas fa-code"
+            skills={frontendSkills}
+            description="Nullam urna sem, vehicula vitae viverra vel, eleifend vel urna. Ut
           vulputate dui a dui tempus, sit amet gravida odio suscipit. Aliquam
           quis eros et dui dignissim.
+          "
+            color="#36aaff"
+          />
         </div>
-        <div className={styles.skill_list}>
-          {frontendSkills.map((skill) => (
-            <div className={styles.list_item}>
-              <div className={styles.dot}></div>
-              <div>{skill}</div>
-            </div>
-          ))}
-        </div>
-      </div>
-      <div className={styles.feature_item}>
-        <i className="fas fa-terminal"></i>
-        <h3>Back End</h3>
-        <div className={styles.description}>
-          Nullam urna sem, vehicula vitae viverra vel, eleifend vel urna. Ut
+        <div>
+          <FeatureItem
+            title="Back End"
+            icon="fas fa-terminal"
+            skills={backendSkills}
+            description="Nullam urna sem, vehicula vitae viverra vel, eleifend vel urna. Ut
           vulputate dui a dui tempus, sit amet gravida odio suscipit. Aliquam
           quis eros et dui dignissim.
+          "
+            color="#30E5AD"
+          />
         </div>
-      </div>
-      <div className={styles.feature_item}>
-        <i className="fab fa-apple"></i>
-        <h3>iOS development</h3>
-        <div className={styles.description}>
-          Nullam urna sem, vehicula vitae viverra vel, eleifend vel urna. Ut
+        <div>
+          <FeatureItem
+            title="iOS development"
+            icon="fab fa-apple"
+            skills={iosSkills}
+            description="Nullam urna sem, vehicula vitae viverra vel, eleifend vel urna. Ut
           vulputate dui a dui tempus, sit amet gravida odio suscipit. Aliquam
           quis eros et dui dignissim.
+          "
+            color="#838383"
+          />
         </div>
-      </div>
+      </Swiper>
     </div>
   );
 };
