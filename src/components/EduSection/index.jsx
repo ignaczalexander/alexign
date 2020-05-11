294982390d46ae61ccc803946a1a507d5c22da0f
Alexander Ignacz <ignaczalexander@gmail.com>
2020-05-11 14:07:15 +0200

    Add education section

---
+++ src/components/EduSection/index.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import styles from "./edu.module.scss";
+import Titles from "../shared/Titles";
+import ScrollAnimation from "react-animate-on-scroll";
+import "animate.css/animate.min.css";
+
+const EduSection = () => {
+  return (
+    <div id="education" className={styles.container}>
+      <ScrollAnimation
+        duration={0.7}
+        animateOnce
+        animateIn="animate__fadeInRight"
+        offset={0}
+      >
+        <Titles title="Education" icon="fas fa-graduation-cap" />
+      </ScrollAnimation>
+      <div className={styles.edu_list}>
+        <ScrollAnimation
+          duration={0.7}
+          animateOnce
+          animateIn="animate__fadeInLeft"
+        >
+          <div className={styles.edu_item}>
+            <div className={styles.degree}>Bachelor's degree</div>
+            <div className={styles.name}>Software Development</div>
+            <div className={styles.school}>
+              University College of Northern Denmark
+            </div>
+          </div>
+          <div className={styles.edu_item}>
+            <div className={styles.degree}>Academy Profession degree</div>
+            <div className={styles.name}>Computer Science</div>
+            <div className={styles.school}>
+              University College of Northern Denmark
+            </div>
+          </div>
+        </ScrollAnimation>
+      </div>
+    </div>
+  );
+};
+
+export default EduSection;
