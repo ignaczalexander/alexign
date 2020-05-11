84a4299fee0c936f49840471f94b1e7b6acecccc
Alexander Ignacz <ignaczalexander@gmail.com>
2020-05-11 22:54:48 +0200

    Add animateOnce prop

--- src/components/shared/ScrollAnimation/index.jsx
+++ src/components/shared/ScrollAnimation/index.jsx
@@ -3,7 +3,11 @@ import ScrollAnim from "react-animate-on-scroll";
 import "animate.css/animate.min.css";
 
 const ScrollAnimation = (props) => {
-  return <ScrollAnim {...props}>{props.children}</ScrollAnim>;
+  return (
+    <ScrollAnim animateOnce {...props}>
+      {props.children}
+    </ScrollAnim>
+  );
 };
 
 export default ScrollAnimation;
