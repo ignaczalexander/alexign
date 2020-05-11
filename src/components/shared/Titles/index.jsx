25739a289276f238e98a4c47b9e060645d84ed3d
Alexander Ignacz <ignaczalexander@gmail.com>
2020-05-11 14:10:07 +0200

    Add icon prop

--- src/components/shared/Titles/index.jsx
+++ src/components/shared/Titles/index.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 import styles from "./titles.module.scss";
 
-const Titles = props => {
-  const { title, subtitle } = props;
+const Titles = (props) => {
+  const { title, subtitle, icon } = props;
   return (
     <div className={styles.container}>
+      {icon && <i className={icon}></i>}
       <h2>{title}</h2>
       {subtitle && <h3>{subtitle}</h3>}
     </div>
   );
+};
+Titles.defaultProps = {
+  icon: "",
 };
 
 export default Titles;
