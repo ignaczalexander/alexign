80dfadd582db39337cad4ed72431bcef86307410
Alexander Ignacz <ignaczalexander@gmail.com>
2020-05-12 10:21:28 +0200

    Add fontawesome components

---
+++ src/fontawesome/index.js
@@ -0,0 +1,26 @@
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faCode,
+  faTerminal,
+  faGraduationCap,
+  faBriefcase,
+  faTimes,
+  faBars,
+} from "@fortawesome/free-solid-svg-icons";
+import {
+  faApple,
+  faGithub,
+  faLinkedin,
+} from "@fortawesome/free-brands-svg-icons";
+
+library.add(
+  faCode,
+  faTerminal,
+  faApple,
+  faGraduationCap,
+  faBriefcase,
+  faTimes,
+  faBars,
+  faGithub,
+  faLinkedin
+);
--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import WebFont from "webfontloader";
 
+import "./fontawesome";
+
 WebFont.load({
   google: {
     families: ["Source Sans Pro:300,400,600,700", "sans-serif"],
