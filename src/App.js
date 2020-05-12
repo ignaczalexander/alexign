66020c738b261be7a36323b11e373cb13a96426c
Alexander Ignacz <ignaczalexander@gmail.com>
2020-05-12 10:20:02 +0200

    Remove unused lines

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
 import { Home } from "./components/Home";
-import { Navbar } from "./components/Navbar";
 
 function App() {
   return (
     <Router>
-      {/* <Navbar /> */}
       <Route path="/">
         <Home />
       </Route>
